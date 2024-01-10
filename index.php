4c0dc2cdb73bda9ce843b4609918a56d0dd08837
IslemMedjahdi <ki_medjahdi@esi.dz>
2024-01-10 15:15:55 +0100

    Update page title in index.php

--- index.php
+++ index.php
@@ -4,17 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Carcompass</title>
-    <link rel="stylesheet" href="/cars-comparer-2cs-project/css/main.css">
-    <link rel="stylesheet" href="/cars-comparer-2cs-project/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-
     <?php
     require_once(__DIR__ . "/controllers/SettingsController.php");
     $settingsController = new SettingsController();
     $faviconUrl = $settingsController->getFavicon()['data']["faviconUrl"] ?? "/assets/images/favicon.png";
     $primaryColor = $settingsController->getPrimaryColor()['data']["primary_color"] ?? "#0d6efd";
+    $title = $settingsController->getContent()['data']["title"] ?? "Carcompass";
     ?>
+    <title>
+        <?= $title ?>
+    </title>
+    <link rel="stylesheet" href="/cars-comparer-2cs-project/css/main.css">
+    <link rel="stylesheet" href="/cars-comparer-2cs-project/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
     <link rel="icon" type="image/x-icon" href="/cars-comparer-2cs-project<?= $faviconUrl ?>">
 
 
